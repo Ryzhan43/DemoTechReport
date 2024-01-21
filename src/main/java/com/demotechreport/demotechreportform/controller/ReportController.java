f0ebe9df02aab57f0b84759094d5eba27bab29da
Mykyta Ryzhan <59860730+Ryzhan43@users.noreply.github.com>
2024-01-20 22:04:03 -0500

    Submit report added

--- src/main/java/com/demotechreport/demotechreportform/controller/ReportController.java
+++ src/main/java/com/demotechreport/demotechreportform/controller/ReportController.java
@@ -6,17 +6,24 @@ import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/report")
 public class ReportController {
-
-    @GetMapping("/create")
+   @GetMapping("/create")
    private String createReport(Model model){
         model.addAttribute("weekdays", Weekday.values());
         model.addAttribute("report", new ReportDTO());
        return "/report/create-report";
+   }
+
+   @PostMapping("/submit-create-report")
+   private String saveReport(@ModelAttribute("report") ReportDTO reportDTO){
+       System.out.println(reportDTO.toString());
+       return "redirect:/report/create";
    }
 
 }
