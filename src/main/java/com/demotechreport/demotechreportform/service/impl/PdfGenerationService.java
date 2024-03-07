89b08b852782dd5e96d1573440cfae8a845788c7
Mykyta Ryzhan <59860730+Ryzhan43@users.noreply.github.com>
2024-03-06 21:56:10 -0500

    Updated savePdf

--- src/main/java/com/demotechreport/demotechreportform/service/impl/PdfGenerationService.java
+++ src/main/java/com/demotechreport/demotechreportform/service/impl/PdfGenerationService.java
@@ -34,20 +34,6 @@ public class PdfGenerationService {
     }
 
 
-//
-//    public byte[] generatePdf(Long id) throws IOException {
-//        Document document = new Document();
-//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-//        PdfWriter.getInstance(document, byteArrayOutputStream);
-//
-//        document.open();
-//        document.add(new Paragraph("PDF Content: " + reportService.findReportById(id).toPdfString()));
-//        document.close();
-//
-//        return byteArrayOutputStream.toByteArray();
-//    }
-
-
     public byte[] generatePdf(Long id) throws IOException {
         File oldFile = new File("src/main/resources/demo-tech-form.pdf");
         PDDocument document = PDDocument.load(oldFile);
@@ -262,69 +248,6 @@ public class PdfGenerationService {
         contentStream.closePath();
         contentStream.setStrokingColor(255, 0, 0); // Set stroke color to red
         contentStream.stroke();
-}
-
-
-    public byte[] generatePdfWithTable() throws IOException {
-        try (PDDocument document = new PDDocument()) {
-            PDPage page = new PDPage(PDRectangle.A4);
-            document.addPage(page);
-
-            try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
-                float margin = 50;
-                float yStart = page.getMediaBox().getHeight() - margin;
-                float tableWidth = page.getMediaBox().getWidth() - 2 * margin;
-                float yPosition = yStart;
-                float tableHeight = 100f; // Adjust as needed
-                float marginX = 5f;
-
-                int rows = 5; // Number of rows in the table
-                int cols = 3; // Number of columns in the table
-                float rowHeight = 20f;
-                float tableWidthMargin = tableWidth / cols;
-                float tableHeightMargin = tableHeight / rows;
-
-                // Draw table headers
-
-                contentStream.setFont(PDType1Font.TIMES_ROMAN, 12);
-                float nextX = marginX;
-                float nextY = yPosition;
-                for (int i = 0; i < cols; i++) {
-                    contentStream.beginText();
-                    contentStream.newLineAtOffset(nextX, nextY);
-                    contentStream.showText("Header " + (i + 1));
-                    contentStream.endText();
-                    nextX += tableWidthMargin;
-                }
-
-                // Draw table rows
-                nextY -= rowHeight;
-                for (int i = 0; i < rows; i++) {
-                    nextX = marginX;
-                    contentStream.setLineWidth(1f);
-                    contentStream.moveTo(marginX, nextY);
-                    contentStream.lineTo(marginX + tableWidth, nextY);
-                    contentStream.stroke();
-
-                    for (int j = 0; j < cols; j++) {
-                        contentStream.beginText();
-                        contentStream.newLineAtOffset(nextX, nextY - 15); // Adjust vertical alignment
-                        contentStream.showText("Data " + (i + 1) + "," + (j + 1));
-                        contentStream.endText();
-                        nextX += tableWidthMargin;
-                    }
-
-                    nextY -= rowHeight;
-                }
-
-                contentStream.close();
-            }
-
-
-
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            document.save(outputStream);
-            return outputStream.toByteArray();
-        }
     }
+
 }
